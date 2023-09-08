24da8904acd792f5215bc489933df71ab52eefcf
Allison Fiting <allison@Allisons-MacBook-Pro.local>
2023-09-08 18:37:01 -0400

    adding house page

--- projects/projectJs/procrastination.js
+++ projects/projectJs/procrastination.js
@@ -1,7 +1,8 @@
 let taskArray = ['do dishes', 'vacuum', 'read a book', 'clean the fridge', 
-'clean the living room', 'clean the bathroom'];
+'clean the living room', 'clean the bathroom', 'face your inner demons', 'throw out sh*t you don\'t need', 'get crafty'];
 let timeArray = ['10 minutes', '15 minutes', '20 minutes', '25 minutes', '30 minutes']
-let rewardArray = ['nap', 'long walk', 'healthy snack', 'highfive', 'hot beverage']
+let rewardArray = ['a nap', 'a long walk', 'a healthy snack', 'a highfive', 'a hot beverage', 'social interation with a kitty', 'buying sh*t you don\'t need'
+]
 let goButton = document.getElementById('procButton');
 let target = document.getElementById('procTarget');
 function chooseTask() {
@@ -21,7 +22,7 @@ function procrastinate() {
     let taskChoice = chooseTask();
     let timeChoice = chooseTime();
     let rewardChoice = chooseReward();
-    let text = `Go ${taskChoice} for ${timeChoice} then reward youself with a ${rewardChoice}.`;
+    let text = `Go ${taskChoice} for ${timeChoice} then reward yourself with ${rewardChoice}. `;
     target.innerHTML = text;
 };
 
